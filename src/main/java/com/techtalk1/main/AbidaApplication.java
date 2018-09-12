cd8d0fa03648478c116c892963949a3b2290d8c2
Fahmy Abida <fahmyabida@gmail.com>
2018-09-12 13:57:07 +0700

    Init Commit

---
+++ src/main/java/com/techtalk1/main/AbidaApplication.java
@@ -0,0 +1,12 @@
+package com.techtalk1.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbidaApplication.class, args);
+	}
+}
